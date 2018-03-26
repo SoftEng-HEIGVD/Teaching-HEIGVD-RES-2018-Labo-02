b2ab2d82ca51fd8068bb43784faa4284b2915801
Unknown <james.smith@heig-vd.ch>
2018-03-27 00:14:05 +0200

    Add third test and implementation: server and client protoc v2

--- QuizRouletteServer-build/QuizRouletteServer-test/src/test/java/ch/heigvd/res/labs/roulette/net/client/RouletteV2SmithHeigTest.java
+++ QuizRouletteServer-build/QuizRouletteServer-test/src/test/java/ch/heigvd/res/labs/roulette/net/client/RouletteV2SmithHeigTest.java
@@ -1,14 +1,22 @@
 package ch.heigvd.res.labs.roulette.net.client;
 
 import ch.heigvd.res.labs.roulette.data.EmptyStoreException;
+import ch.heigvd.res.labs.roulette.data.JsonObjectMapper;
 import ch.heigvd.res.labs.roulette.data.Student;
+import ch.heigvd.res.labs.roulette.net.protocol.ByeCommandResponse;
 import ch.heigvd.res.labs.roulette.net.protocol.RouletteV1Protocol;
 import ch.heigvd.res.labs.roulette.net.protocol.RouletteV2Protocol;
 import ch.heigvd.schoolpulse.TestAuthor;
 import org.junit.*;
 import org.junit.rules.ExpectedException;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 import static org.junit.Assert.*;
@@ -32,18 +40,28 @@ public class RouletteV2SmithHeigTest {
     @Test
     @TestAuthor(githubId = "smithheig")
     public void testClientReceivedCorrectListOfStudentFromServer() throws IOException {
-        roulettePair.getClient().loadStudent("bob");
-        roulettePair.getClient().loadStudent("john");
-        Student bob = new Student("bob");
-        Student john = new Student("john");
-
         IRouletteV2Client client = (IRouletteV2Client)roulettePair.getClient();
+        client.loadStudent("bob");
+        client.loadStudent("john");
 
-        List<Student> students = client.listStudents();
+        assertTrue(client.listStudents().contains(new Student("bob")));
+        assertTrue(client.listStudents().contains(new Student("john")));
+    }
 
-        assertTrue(students.contains(bob));
-        assertTrue(students.contains(john));
-        assertEquals(students.size(),2);
+    @Test
+    @TestAuthor(githubId = "smithheig")
+    public void testResponseFromServerWhenExitWithBye() throws IOException{
+        Socket client = new Socket("localhost",roulettePair.getServer().getPort());
+        BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+        PrintWriter out = new PrintWriter(client.getOutputStream());
+
+        in.readLine();
+
+        out.println("BYE");
+        out.flush();
+
+        String s = in.readLine();
+        assertEquals(s, "{\"status\":\"success\",\"nbCommands\":1}");
     }
 
 }
