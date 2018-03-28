86166116727b4b4bc7f2030da68a3c4b92f6d60b
Labinot Rashiti <labinot.rashiti@heig-vd.ch>
2018-03-28 11:52:47 +0200

    Implementation updated and methods loadStudents added

--- QuizRouletteServer-build/QuizRouletteServer-code/src/main/java/ch/heigvd/res/labs/roulette/net/client/RouletteV1ClientImpl.java
+++ QuizRouletteServer-build/QuizRouletteServer-code/src/main/java/ch/heigvd/res/labs/roulette/net/client/RouletteV1ClientImpl.java
@@ -19,112 +19,109 @@ import java.util.logging.Logger;
 
 /**
  * This class implements the client side of the protocol specification (version 1).
- * 
+ *
  * @author Olivier Liechti
  * @author Labinot Rashiti
  */
 public class RouletteV1ClientImpl implements IRouletteV1Client {
 
-  private static final Logger LOG = Logger.getLogger(RouletteV1ClientImpl.class.getName());
+   private static final Logger LOG = Logger.getLogger(RouletteV1ClientImpl.class.getName());
 
-  Socket clientSocket = null;
-  BufferedReader in = null;
-  PrintWriter out = null;
-  boolean connected = false;
-  
-  @Override
-  public void connect(String server, int port) throws IOException {
-     try {
-        Socket clientSocket = new Socket(server, port);
-        
-	in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-	out = new PrintWriter(clientSocket.getOutputStream());
-        connected = true;
-      	out.println();
-        out.flush();
-        
-     } catch (IOException e) {
-        LOG.log(Level.SEVERE, e.getMessage(), e);
-     }
-  }
+   Socket clientSocket = null;
+   BufferedReader in = null;
+   PrintWriter out = null;
+   boolean connected = false;
 
-  @Override
-  public void disconnect() throws IOException {
-     try {
-        if (in != null) {
-           in.close();
-        }
-     } catch (IOException ex) {
-        LOG.log(Level.SEVERE, ex.getMessage(), ex);
-     }
+   @Override
+   public void connect(String server, int port) throws IOException {
+      try {
+         Socket clientSocket = new Socket(server, port);
 
-     if (out != null) {
-        out.close();
-     }
+         in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+         out = new PrintWriter(clientSocket.getOutputStream());
+         connected = true;
+         out.println();
+         out.flush();
+         in.readLine();
+      } catch (IOException e) {
+         LOG.log(Level.SEVERE, e.getMessage(), e);
+      }
+   }
 
-     try {
-        if (clientSocket != null) {
-           clientSocket.close();
-        }
-     } catch (IOException ex) {
-        LOG.log(Level.SEVERE, ex.getMessage(), ex);
-     }
-  }
+   @Override
+   public void disconnect() throws IOException {
+      try {
+         if (in != null) {
+            in.close();
+         }
+      } catch (IOException ex) {
+         LOG.log(Level.SEVERE, ex.getMessage(), ex);
+      }
 
-  @Override
-  public boolean isConnected() {
+      if (out != null) {
+         out.close();
+      }
+
+      try {
+         if (clientSocket != null) {
+            clientSocket.close();
+         }
+      } catch (IOException ex) {
+         LOG.log(Level.SEVERE, ex.getMessage(), ex);
+      }
+   }
+
+   @Override
+   public boolean isConnected() {
       return connected;
-  }
+   }
 
-  @Override
-  public void loadStudent(String fullname) throws IOException {
-    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-  }
+   @Override
+   public void loadStudent(String fullname) throws IOException {
+      out.println(RouletteV1Protocol.CMD_LOAD);
+      out.flush();
+      in.readLine(); // show the answer of the server
+      out.println(fullname);
+      out.flush();
+      out.println(RouletteV1Protocol.CMD_LOAD_ENDOFDATA_MARKER);
+      out.flush();
+      in.readLine(); // show the answer of the server
+   }
 
-  @Override
-  public void loadStudents(List<Student> students) throws IOException {
-    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-  }
+   @Override
+   public void loadStudents(List<Student> students) throws IOException {
+      for (Student student : students) {
+         loadStudent(student.getFullname());
+      }
+   }
 
-  @Override
-  public Student pickRandomStudent() throws EmptyStoreException, IOException {
-     try {
-        out.println("RANDOM");
-        out.flush();
-        String response = in.readLine();
-        RandomCommandResponse info = JsonObjectMapper.parseJson(response, RandomCommandResponse.class);
-        return new Student(info.getFullname());
-     } catch (IOException e) {
-        LOG.log(Level.SEVERE, e.getMessage(), e);
-        return null;
-     }
-  }
+   @Override
+   public Student pickRandomStudent() throws EmptyStoreException, IOException {
+      out.println(RouletteV1Protocol.CMD_RANDOM);
+      out.flush();
+      String response = in.readLine();
+      RandomCommandResponse info = JsonObjectMapper.parseJson(response, RandomCommandResponse.class);
+      if (info.getError() != null) {
+         throw new EmptyStoreException();
+      }
+      return new Student(info.getFullname());
+   }
 
-  @Override
-  public int getNumberOfStudents() throws IOException {
-     try {
-        out.println("INFO");
-        out.flush();
-        String response = in.readLine();
-        InfoCommandResponse info = JsonObjectMapper.parseJson(response, InfoCommandResponse.class);
-        return info.getNumberOfStudents();
-     } catch (IOException e) {
-        LOG.log(Level.SEVERE, e.getMessage(), e);
-        return -1;
-     }
-  }
+   @Override
+   public int getNumberOfStudents() throws IOException {
+      out.println(RouletteV1Protocol.CMD_INFO);
+      out.flush();
+      String response = in.readLine();
+      InfoCommandResponse info = JsonObjectMapper.parseJson(response, InfoCommandResponse.class);
+      return info.getNumberOfStudents();
+   }
 
-  @Override
-  public String getProtocolVersion() throws IOException {
-     try {
-        out.println("INFO");
-        out.flush();
-        String response = in.readLine();
-        InfoCommandResponse info = JsonObjectMapper.parseJson(response, InfoCommandResponse.class);
-        return info.getProtocolVersion();
-     } catch (IOException e) {
-        LOG.log(Level.SEVERE, e.getMessage(), e);
-        return "";
-     }
-  }
+   @Override
+   public String getProtocolVersion() throws IOException {
+      out.println(RouletteV1Protocol.CMD_INFO);
+      out.flush();
+      String response = in.readLine();
+      InfoCommandResponse info = JsonObjectMapper.parseJson(response, InfoCommandResponse.class);
+      return info.getProtocolVersion();
+   }
 }
