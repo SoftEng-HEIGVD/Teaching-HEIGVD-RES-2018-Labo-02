0dea68b37deb4ec20aa04f5eac6718b90681532e
Daniel Gonzalez Lopez <idaniel.gonzalezlopezi@gmail.com>
2018-04-01 23:07:21 +0200

    3 tests for V2 added, waiting for telegram response before adding some more (maybe)

--- QuizRouletteServer-build/QuizRouletteServer-test/src/test/java/ch/heigvd/res/labs/roulette/net/client/RouletteV2AngoranceTest.java
+++ QuizRouletteServer-build/QuizRouletteServer-test/src/test/java/ch/heigvd/res/labs/roulette/net/client/RouletteV2AngoranceTest.java
@@ -1,86 +1,88 @@
-    package ch.heigvd.res.labs.roulette.net.client;
+package ch.heigvd.res.labs.roulette.net.client;
 
-    import ch.heigvd.res.labs.roulette.data.Student;
-    import ch.heigvd.res.labs.roulette.net.protocol.RouletteV2Protocol;
-    import ch.heigvd.schoolpulse.TestAuthor;
-    import org.junit.Rule;
-    import org.junit.Test;
-    import org.junit.rules.ExpectedException;
+import ch.heigvd.res.labs.roulette.data.Student;
+import ch.heigvd.res.labs.roulette.net.protocol.RouletteV2Protocol;
+import ch.heigvd.schoolpulse.TestAuthor;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
-    import java.io.IOException;
-    import java.util.*;
+import java.io.IOException;
+import java.util.*;
 
-    import static org.junit.Assert.assertEquals;
-    import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
-    /**
-    * This class contains automated tests to validate the client and the server
-    * implementation of the Roulette Protocol (version 2)
-    *
-    * @author Olivier Liechti
-    */
-    public class RouletteV2AngoranceTest {
-
+/**
+ * This class contains automated tests to validate the client and the server
+ * implementation of the Roulette Protocol (version 2)
+ *
+ * @author Olivier Liechti
+ *
+ * @modifiedBy Daniel Gonzalez Lopez, Héléna Line Reymond
+ */
+public class RouletteV2AngoranceTest {
+    
     @Rule
     public ExpectedException exception = ExpectedException.none();
-
+    
     @Rule
     public EphemeralClientServerPair roulettePair = new EphemeralClientServerPair(RouletteV2Protocol.VERSION);
-
+    
     @Test
     @TestAuthor(githubId = {"Angorance", "LNAline"})
     public void theServerShouldClearStudents() throws IOException {
-
-        // Create the client from the informations of the server
+        
+        // Create the client from the information of the server
         int port = roulettePair.getServer().getPort();
         IRouletteV2Client client = new RouletteV2ClientImpl();
         client.connect("localhost", port);
-
+        
         // Add students on the server
-        List<Student> serverStudent = new ArrayList<>();
-
-        serverStudent.add(new Student("Sacha"));
-        serverStudent.add(new Student("Olivier"));
-        serverStudent.add(new Student("Fabienne"));
-
-        for(Student s : serverStudent) {
-            client.loadStudent(s.getFullname());
-        }
-
+        List<Student> serverStudents = new ArrayList<>();
+        
+        serverStudents.add(new Student("Sacha"));
+        serverStudents.add(new Student("Olivier"));
+        serverStudents.add(new Student("Fabienne"));
+        
+        client.loadStudents(serverStudents);
+        
         // Clear the list of students from the client
-        assertEquals(serverStudent.size(), client.getNumberOfStudents());
+        assertEquals(serverStudents.size(), client.getNumberOfStudents());
         client.clearDataStore();
         assertEquals(0, client.getNumberOfStudents());
     }
-
+    
     @Test
     @TestAuthor(githubId = {"Angorance", "LNAline"})
     public void theServerShouldListStudents() throws IOException {
-
+        
         // Create the client from the informations of the server
         int port = roulettePair.getServer().getPort();
         IRouletteV2Client client = new RouletteV2ClientImpl();
         client.connect("localhost", port);
-
+        
         // Add students on the server
-        List<Student> serverStudent = new ArrayList<>();
-
-        serverStudent.add(new Student("Sacha"));
-        serverStudent.add(new Student("Olivier"));
-        serverStudent.add(new Student("Fabienne"));
-
-        for(Student s : serverStudent) {
-            client.loadStudent(s.getFullname());
-        }
-
+        List<Student> serverStudents = new ArrayList<>();
+        
+        serverStudents.add(new Student("Sacha"));
+        serverStudents.add(new Student("Olivier"));
+        serverStudents.add(new Student("Fabienne"));
+    
+        client.loadStudents(serverStudents);
+        
         // Get the list of students from the client
         List<Student> clientStudents = client.listStudents();
-
+        
         // Check if the list of student is the same on client and server
-        assertEquals(serverStudent.size(), client.getNumberOfStudents());
-
-        for(Student s : serverStudent){
-            assertTrue(clientStudents.contains(s));
-        }
+        assertEquals(serverStudents.size(), client.getNumberOfStudents());
+        
+        assertTrue(clientStudents.containsAll(serverStudents));
+    }
+    
+    @Test
+    @TestAuthor(githubId = {"Angorance", "LNAline"})
+    public void theServerShouldReturnTheCorrectVersionNumber() throws IOException {
+        assertEquals(RouletteV2Protocol.VERSION, roulettePair.getClient().getProtocolVersion());
     }
 }
