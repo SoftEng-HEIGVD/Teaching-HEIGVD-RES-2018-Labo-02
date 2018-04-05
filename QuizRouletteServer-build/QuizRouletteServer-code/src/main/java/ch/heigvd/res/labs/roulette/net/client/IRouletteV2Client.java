da2402d77ca3cc9d691aeae7d3cbb75c306b3093
nyahon <yohann.meyer@gmail.com>
2018-04-05 15:22:53 +0200

    all good for compilation, included function names of tests added by others

--- QuizRouletteServer-build/QuizRouletteServer-code/src/main/java/ch/heigvd/res/labs/roulette/net/client/IRouletteV2Client.java
+++ QuizRouletteServer-build/QuizRouletteServer-code/src/main/java/ch/heigvd/res/labs/roulette/net/client/IRouletteV2Client.java
@@ -28,4 +28,11 @@ public interface IRouletteV2Client extends IRouletteV1Client {
    */
   public List<Student> listStudents() throws IOException;
 
+  public int getNumberOfStudentAdded();
+
+   public int getNumberOfCommands();
+
+   public boolean checkSuccessOfCommand();
+
 }
+
