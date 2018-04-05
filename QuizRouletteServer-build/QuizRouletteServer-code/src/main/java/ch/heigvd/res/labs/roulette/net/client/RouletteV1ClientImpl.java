a3df0feebab4d06645e3c9ead4f1f32d7cd884e1
nyahon <yohann.meyer@gmail.com>
2018-04-05 15:50:36 +0200

    recuperation of V1impl

--- QuizRouletteServer-build/QuizRouletteServer-code/src/main/java/ch/heigvd/res/labs/roulette/net/client/RouletteV1ClientImpl.java
+++ QuizRouletteServer-build/QuizRouletteServer-code/src/main/java/ch/heigvd/res/labs/roulette/net/client/RouletteV1ClientImpl.java
@@ -15,100 +15,126 @@ import java.util.logging.Logger;
 
 /**
  * This class implements the client side of the protocol specification (version 1).
- *
+ * 
  * @author Olivier Liechti
  */
 public class RouletteV1ClientImpl implements IRouletteV1Client {
 
-  private enum CONNECT {
-    RANDOM ("RANDOM"),
-    LOAD ("LOAD"),
-    HELP ("HELP"),
-    INFO ("INFO"),
-    BYE  ("BYE"),
-    ENDOFDATA ("ENDOFDATA");
+    private enum CONNECT {
+        RANDOM ("RANDOM"),
+        LOAD ("LOAD"),
+        HELP ("HELP"),
+        INFO ("INFO"),
+        BYE  ("BYE"),
+        ENDOFDATA ("ENDOFDATA");
 
-    private final String name;
-    private CONNECT(String s) {
-      name = s;
-    }
+        private final String name;
+        private CONNECT(String s) {
+            name = s;
+        }
 
-    public String toString() {
-      return this.name;
-    }}
+        public String toString() {
+            return this.name;
+        }}
 
-  private static final Logger LOG = Logger.getLogger(RouletteV1ClientImpl.class.getName());
-  private BufferedReader is;
-  private PrintWriter os;
-  private Socket socket;
+    private static final Logger LOG = Logger.getLogger(RouletteV1ClientImpl.class.getName());
+    private BufferedReader is;
+    private PrintWriter os;
+    private Socket socket;
 
 
-  @Override
+    @Override
   public void connect(String server, int port) throws IOException {
-    socket = new Socket(server, port);
-    is = new BufferedReader( new InputStreamReader( socket.getInputStream() ) );
-    os = new PrintWriter( socket.getOutputStream() );
-    LOG.info("Connected to server " + server + " on port " + port + "." );
+      socket = new Socket(server, port);
+      is = new BufferedReader( new InputStreamReader( socket.getInputStream() ) );
+      os = new PrintWriter( socket.getOutputStream() );
+   //   String resp = is.readLine();
+   //   System.out.println( resp );
+      LOG.info("Connected to server " + server + " on port " + port + "." );
 
   }
 
   @Override
   public void disconnect() throws IOException {
-    is.close();
-    os.close();
-    socket.close();
-    LOG.info("Disconnected from server.");
+      is.close();
+      os.close();
+      socket.close();
+      LOG.info("Disconnected from server.");
   }
 
   @Override
   public boolean isConnected() {
-    return socket.isConnected();
+
+
+        return socket != null;
+
+
   }
 
   @Override
   public void loadStudent(String fullname) throws IOException {
-    LOG.info("Loading " + fullname + "on server...");
+      LOG.info("Loading " + fullname + "on server...");
 
-    os.println(RouletteV1Protocol.CMD_LOAD);
-    os.println(fullname);
-    os.println(RouletteV1Protocol.CMD_LOAD_ENDOFDATA_MARKER);
-    os.flush();
+      os.println(CONNECT.LOAD.toString());
+      os.println(fullname);
+      os.println(CONNECT.ENDOFDATA.toString());
+      os.flush();
 
-    LOG.info("Loaded");
+      LOG.info("Loaded");
 
   }
 
   @Override
   public void loadStudents(List<Student> students) throws IOException {
-    LOG.info("Loading list of Students on server...");
+      LOG.info("Loading list of Students on server...");
 
-    os.println(CONNECT.LOAD.toString());
-    for ( Student s: students) {
-      os.println( s.toString() );
-    }
-    os.println(CONNECT.ENDOFDATA.toString());
-    os.flush();
+      os.println(CONNECT.LOAD.toString());
+      for ( Student s: students) {
+          os.println( s.toString() );
+      }
+      os.println(CONNECT.ENDOFDATA.toString());
+      os.flush();
 
-    LOG.info("Loaded");
+      LOG.info("Loaded");
   }
 
   @Override
   public Student pickRandomStudent() throws EmptyStoreException, IOException {
 
-    os.println(CONNECT.RANDOM.toString());
-    String rep = is.readLine();
 
-    return Student.fromJson( JsonObjectMapper.parseJson(rep, String.class) );
+      os.println(CONNECT.RANDOM.toString());
+      String rep = is.readLine();
+      if(getNumberOfStudents() == 0)
+          throw new EmptyStoreException();
+      return Student.fromJson( JsonObjectMapper.parseJson(rep, String.class) );
   }
 
   @Override
   public int getNumberOfStudents() throws IOException {
-    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+
+      os.println(CONNECT.INFO.toString());
+      os.flush();
+      String rep = is.readLine();
+    /*  JSONObject jsonObj = new JSONObject(rep);
+      if( !jsonObj.has("numberOfStudents")){
+          return 0;
+      }
+      return Integer.parseInt( jsonObj.getString("numberOfStudents") );
+
+*/
+    return 0;
   }
 
   @Override
   public String getProtocolVersion() throws IOException {
-    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+
+      os.println(CONNECT.INFO.toString());
+      os.flush();
+      String rep = is.readLine();
+  /*    JSONObject jsonObj = new JSONObject(rep);
+      return jsonObj.getString("protocolVersion");
+      */
+    return "off";
   }
 
 
