f1cd802fa82d2956e30e10fb123f198a5639bd08
François <francois.burgener@hotmail.fr>
2018-04-05 13:09:42 +0200

    .

--- QuizRouletteServer-build/QuizRouletteServer-test/src/test/java/ch/heigvd/res/labs/roulette/net/client/RouletteV2WasadigiTest.java
+++ QuizRouletteServer-build/QuizRouletteServer-test/src/test/java/ch/heigvd/res/labs/roulette/net/client/RouletteV2WasadigiTest.java
@@ -1,11 +1,7 @@
 package ch.heigvd.res.labs.roulette.net.client;
 
 import ch.heigvd.res.labs.roulette.net.protocol.RouletteV2Protocol;
-import ch.heigvd.schoolpulse.TestAuthor;
-import java.io.IOException;
-import static org.junit.Assert.assertTrue;
 import org.junit.Rule;
-import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
 /**
@@ -16,16 +12,12 @@ import org.junit.rules.ExpectedException;
  */
 public class RouletteV2WasadigiTest {
 
-    @Rule
-    public ExpectedException exception = ExpectedException.none();
+  @Rule
+  public ExpectedException exception = ExpectedException.none();
 
-    @Rule
-    public EphemeralClientServerPair roulettePair = new EphemeralClientServerPair(RouletteV2Protocol.VERSION);
+  @Rule
+  public EphemeralClientServerPair roulettePair = new EphemeralClientServerPair(RouletteV2Protocol.VERSION);
 
-    @Test
-    @TestAuthor(githubId = "wasadigi")
-    public void theTestRouletteServerShouldRunDuringTests() throws IOException {
-        assertTrue(roulettePair.getServer().isRunning());
-    }
 
+  
 }
