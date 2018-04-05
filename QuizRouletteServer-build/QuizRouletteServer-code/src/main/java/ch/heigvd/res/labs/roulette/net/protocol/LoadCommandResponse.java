1f8d9cd922c709ba5c12572a9f193a7b6aaf6f41
joelschar <joel.schar@heig-vd.ch>
2018-04-05 15:59:39 +0200

    ajout entete loadCommandResponse

--- QuizRouletteServer-build/QuizRouletteServer-code/src/main/java/ch/heigvd/res/labs/roulette/net/protocol/LoadCommandResponse.java
+++ QuizRouletteServer-build/QuizRouletteServer-code/src/main/java/ch/heigvd/res/labs/roulette/net/protocol/LoadCommandResponse.java
@@ -1,5 +1,13 @@
 package ch.heigvd.res.labs.roulette.net.protocol;
 
+/**
+ *  This class is used to serialize/deserialize the response sent by the server
+ *  when processing the "LOAD" command defined in the protocol specification. The
+ *  JsonObjectMapper utility class can use this class.
+ *
+ * @author Yann Lederrey and Joel Schar
+ */
+
 public class LoadCommandResponse {
     private String status;
     private int numberOfNewStudents;
