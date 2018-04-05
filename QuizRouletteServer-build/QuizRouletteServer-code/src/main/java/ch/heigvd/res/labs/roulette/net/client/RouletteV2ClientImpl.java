f437c1a0762bc77b674714f12613f2ad542d5b1b
Labinot Rashiti <labinot.rashiti@heig-vd.ch>
2018-04-05 13:51:22 +0200

    Adding methods to interface RouletteV2Client for the tests, getNumberOfNewStudents and getNumberOfCommands added

--- QuizRouletteServer-build/QuizRouletteServer-code/src/main/java/ch/heigvd/res/labs/roulette/net/client/RouletteV2ClientImpl.java
+++ QuizRouletteServer-build/QuizRouletteServer-code/src/main/java/ch/heigvd/res/labs/roulette/net/client/RouletteV2ClientImpl.java
@@ -4,6 +4,9 @@ import ch.heigvd.res.labs.roulette.data.JsonObjectMapper;
 import ch.heigvd.res.labs.roulette.data.Student;
 import ch.heigvd.res.labs.roulette.data.StudentsList;
 import ch.heigvd.res.labs.roulette.net.protocol.RouletteV2Protocol;
+import ch.heigvd.res.labs.roulette.net.protocol.ByeCommandResponse;
+import ch.heigvd.res.labs.roulette.net.protocol.LoadCommandResponse;
+import ch.heigvd.res.labs.roulette.net.protocol.InfoCommandResponse;
 import java.io.IOException;
 import java.util.List;
 
@@ -13,7 +16,26 @@ import java.util.List;
  * @author Olivier Liechti
  */
 public class RouletteV2ClientImpl extends RouletteV1ClientImpl implements IRouletteV2Client {
+   
+   @Override
+   public int getNumberOfCommands() throws IOException {
+      out.println(RouletteV2Protocol.CMD_BYE);
+      out.flush();
+      String response = in.readLine(); // read the answer from server
+      ByeCommandResponse info = JsonObjectMapper.parseJson(response, ByeCommandResponse.class);
+      return info.getNumberOfCommands();
+   }
 
+   @Override
+   public int getNumberOfStudentAdded() throws IOException {
+      out.println(RouletteV2Protocol.CMD_LOAD);
+      out.flush();
+      String response = in.readLine(); // read the answer from server
+      LoadCommandResponse info = JsonObjectMapper.parseJson(response, LoadCommandResponse.class);
+      return info.getNumberOfNewStudents();
+   }
+   
+   
   @Override
   public void clearDataStore() throws IOException {
 
@@ -25,13 +47,10 @@ public class RouletteV2ClientImpl extends RouletteV1ClientImpl implements IRoule
 
   @Override
   public List<Student> listStudents() throws IOException {
-
-    String list;
-
     out.println(RouletteV2Protocol.CMD_LIST);
     out.flush();
 
-    list = in.readLine(); // read the answer from server
+    String list = in.readLine(); // read the answer from server
 
     // parse the JSON answer from server and get a List<Student> from it
     return JsonObjectMapper.parseJson(list, StudentsList.class).getStudents();
