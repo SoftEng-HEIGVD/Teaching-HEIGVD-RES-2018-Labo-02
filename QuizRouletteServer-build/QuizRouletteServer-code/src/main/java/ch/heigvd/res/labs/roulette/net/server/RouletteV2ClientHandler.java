afa8c0124b55991d9aee9163cb1d543421199749
DDejviDD <dejvid.muaremi@heig-vd.ch>
2018-04-06 18:24:16 +0200

    The v2 handler and his Json mapper should now work.

--- QuizRouletteServer-build/QuizRouletteServer-code/src/main/java/ch/heigvd/res/labs/roulette/net/server/RouletteV2ClientHandler.java
+++ QuizRouletteServer-build/QuizRouletteServer-code/src/main/java/ch/heigvd/res/labs/roulette/net/server/RouletteV2ClientHandler.java
@@ -3,7 +3,10 @@ package ch.heigvd.res.labs.roulette.net.server;
 import ch.heigvd.res.labs.roulette.data.EmptyStoreException;
 import ch.heigvd.res.labs.roulette.data.IStudentsStore;
 import ch.heigvd.res.labs.roulette.data.JsonObjectMapper;
+import ch.heigvd.res.labs.roulette.data.StudentsList;
 import ch.heigvd.res.labs.roulette.net.protocol.InfoCommandResponse;
+import ch.heigvd.res.labs.roulette.net.protocol.ByeCommandResponse;
+import ch.heigvd.res.labs.roulette.net.protocol.LoadCommandResponse;
 import ch.heigvd.res.labs.roulette.net.protocol.RandomCommandResponse;
 import ch.heigvd.res.labs.roulette.net.protocol.RouletteV2Protocol;
 
@@ -16,14 +19,23 @@ import java.util.logging.Logger;
  * This class implements the Roulette protocol (version 2).
  *
  * @author Olivier Liechti
+ *
+ * Implemented by :
+ * @author Dejvid Muaremi
+ * @author Loic Frueh
+ *
  */
 public class RouletteV2ClientHandler implements IClientHandler {
   
   final static Logger LOG = Logger.getLogger(RouletteV1ClientHandler.class.getName());
   
   private final IStudentsStore store;
+  private int commandsCount;
 
-  public RouletteV2ClientHandler(IStudentsStore store) { this.store = store; }
+  public RouletteV2ClientHandler(IStudentsStore store) {
+    this.store = store;
+    this.commandsCount = 0;
+  }
 
   @Override
   public void handleClientConnection(InputStream is, OutputStream os) throws IOException {
@@ -37,6 +49,8 @@ public class RouletteV2ClientHandler implements IClientHandler {
     boolean done = false;
     while (!done && ((command = reader.readLine()) != null)) {
       LOG.log(Level.INFO, "COMMAND: {0}", command);
+      // We suppose that every command is valid.
+      ++commandsCount;
       switch (command.toUpperCase()) {
         case RouletteV2Protocol.CMD_RANDOM:
           RandomCommandResponse rcResponse = new RandomCommandResponse();
@@ -57,22 +71,45 @@ public class RouletteV2ClientHandler implements IClientHandler {
           writer.flush();
           break;
         case RouletteV2Protocol.CMD_LOAD:
+          //todo
+          int tmp_NumberOfStudents = store.getNumberOfStudents();
           writer.println(RouletteV2Protocol.RESPONSE_LOAD_START);
           writer.flush();
-          store.importData(reader);
-          writer.println(RouletteV2Protocol.RESPONSE_LOAD_DONE);
+          
+          try {
+            store.importData(reader);
+          }
+          catch (IOException e){
+            LOG.log(Level.SEVERE, "Cannot import the data from the reader {0}", e.getMessage());
+            LoadCommandResponse loadCommandResponse = new LoadCommandResponse("failure", 0);
+            writer.println(JsonObjectMapper.toJson(loadCommandResponse));
+            writer.flush();
+            throw e;
+          }
+          LoadCommandResponse loadCommandResponse = new LoadCommandResponse("success", store.getNumberOfStudents() - tmp_NumberOfStudents);
+          writer.println(JsonObjectMapper.toJson(loadCommandResponse));
           writer.flush();
           break;
         case RouletteV2Protocol.CMD_BYE:
+          ByeCommandResponse byeCommandResponse = new ByeCommandResponse("success", this.commandsCount);
+          writer.println(JsonObjectMapper.toJson(byeCommandResponse));
+          writer.flush();
           done = true;
           break;
         case RouletteV2Protocol.CMD_CLEAR:
           store.clear();
+          writer.println(RouletteV2Protocol.RESPONSE_CLEAR_DONE);
+          writer.flush();
           break;
         case RouletteV2Protocol.CMD_LIST:
-          
+          StudentsList studentsList= new StudentsList();
+          studentsList.setStudents(store.listStudents());
+          writer.println(JsonObjectMapper.toJson(studentsList));
+          writer.flush();
           break;
         default:
+          // if the command wasn't valid, we correct the command count.
+          --commandsCount;
           writer.println("Huh? please use HELP if you don't know what commands are available.");
           writer.flush();
           break;
